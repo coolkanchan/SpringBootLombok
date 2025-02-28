3bcdca73db9c09f5d8f89b176eebb64067ae8f9a
coolkanchan <coolkanchan.shaw@gmail.com>
2025-02-28 01:47:05 -0600

    initial commit for lambok practice

---
+++ src/main/java/com/example/demo/SpringBootLombokApplication.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLombokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLombokApplication.class, args);
+		Student s = new Student(1,"kanchan",1000.00);
+		
+		Employee e = new Employee(1,"vicky",5000.00);
+		
+		System.out.println(s.getName());
+		System.out.println(e.getSalary());
+	}
+
+}
